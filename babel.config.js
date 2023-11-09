116e19a45e36871d89828b65be05a49014a3ae0f
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2023-11-09 13:14:28 +0100

    chore: babel config

--- babel.config.js
+++ babel.config.js
@@ -19,6 +19,7 @@ module.exports = {
           '@/utils': './src/utils',
           '@/navigation': './src/navigation',
           '@/constants': './src/constants',
+          '@/store': './src/store',
         },
       },
     ],
