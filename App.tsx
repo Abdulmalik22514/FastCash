71bff9b2407a144da673b59ba7a391ab07645f49
Abdulmalik Ibrahim <ibrahimmalik095@gmail.com>
2023-11-09 13:13:37 +0100

    feat: complete store setup

--- App.tsx
+++ App.tsx
@@ -2,13 +2,21 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import MainNavigator from './src/navigation';
 import {PaperProvider} from 'react-native-paper';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistor, store} from '@/store/store';
+// import {persistor, store} from '@/store/store';
 
 function App(): JSX.Element {
   return (
     <PaperProvider>
-      <NavigationContainer>
-        <MainNavigator />
-      </NavigationContainer>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <NavigationContainer>
+            <MainNavigator />
+          </NavigationContainer>
+        </PersistGate>
+      </Provider>
     </PaperProvider>
   );
 }
